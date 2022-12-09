b4aad09590e8893a4343d8aaf820abf911f81b97
MattRehme <7242883+MattRehme@users.noreply.github.com>
2022-12-09 13:59:38 -0700

    Minor edits

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import * as THREE from './node_modules/three';
-import {OrbitControls} from './node_modules/three/examples/jsm/controls/OrbitControls';
+import {OrbitControls} from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 // import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {GLTFLoader} from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
